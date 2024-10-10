401c5ec85a2c764ef291dd6f2ca0db86de3dcef2
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-10-10 23:32:49 +0200

    logging

--- src/ModelingEvolution.VideoStreaming.Tests/VideoAddressTests.cs
+++ src/ModelingEvolution.VideoStreaming.Tests/VideoAddressTests.cs
@@ -21,4 +21,37 @@ public class VideoAddressTests
         va.SourceApi.Should().Be(VideoSourceApi.OpenVidCam);
         va.ToString().Should().Be(url);
     }
+
+    [Fact]
+    public void ComplexUri2()
+    {
+        string url = "shm+mjpeg://localhost/a?tags=elo_melo&resolution=SubHd&file=e%3A%5C1.mp4&camera=1&video-api=OpenVidCam";
+        VideoAddress va = VideoAddress.Parse(url);
+        va.Port.Should().Be(0);
+        va.Codec.Should().Be(VideoCodec.Mjpeg);
+        va.VideoTransport.Should().Be(VideoTransport.Shm);
+        va.Host.Should().Be("localhost");
+        va.StreamName.Should().Be("a");
+        va.Resolution.Should().Be(VideoResolution.SubHd);
+        va.Tags.Should().BeEquivalentTo(["elo_melo"]);
+        va.File.Should().Be("e:\\1.mp4");
+        va.VideoSource.Should().Be(VideoSource.File);
+        va.SourceApi.Should().Be(VideoSourceApi.OpenVidCam);
+        va.ToString().Should().Be(url);
+    }
+    [Fact]
+    public void ComplexUri3()
+    {
+        string url = "shm+mjpeg://localhost/default?video-api=Libcamera";
+        VideoAddress va = VideoAddress.Parse(url);
+        va.Port.Should().Be(0);
+        va.Codec.Should().Be(VideoCodec.Mjpeg);
+        va.VideoTransport.Should().Be(VideoTransport.Shm);
+        va.Host.Should().Be("localhost");
+        va.StreamName.Should().Be("default");
+        va.Resolution.Should().Be(VideoResolution.FullHd);
+        va.VideoSource.Should().Be(VideoSource.Camera);
+        va.SourceApi.Should().Be(VideoSourceApi.Libcamera);
+        va.ToString().Should().Be(url);
+    }
 }
