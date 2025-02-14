aa1fe0584efd49188a31c39e3583bb5f8a849c27
Rafal Maciag <rafal.maciag@modelingevolution.com>
2025-02-14 18:05:59 +0100

    Sha and Bytes

--- src/ModelingEvolution.VideoStreaming/Bytes.cs
+++ src/ModelingEvolution.VideoStreaming/Bytes.cs
@@ -1,12 +1,38 @@
-﻿namespace ModelingEvolution.VideoStreaming;
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+using ProtoBuf;
+
+namespace ModelingEvolution.VideoStreaming;
 
 #pragma warning disable CS4014
+
+public class BytesJsonConverter : JsonConverter<Bytes>
+{
+    public override Bytes Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Number)
+        {
+            long value = reader.GetInt64();
+            return new Bytes(value);
+        }
+
+        throw new JsonException($"Unexpected token {reader.TokenType} when parsing Bytes");
+    }
+
+    public override void Write(Utf8JsonWriter writer, Bytes value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue((long)value);
+    }
+}
+[JsonConverter(typeof(BytesJsonConverter))]
+[ProtoContract]
 public record struct Bytes
 {
     private string? _text;
+    [ProtoMember(1, IsRequired = true)]
     private readonly long _value;
     private readonly sbyte _precision;
-    private Bytes(long value, sbyte precision=1)
+    internal Bytes(long value, sbyte precision=1)
     {
         _value = value;
         _text = value.WithSizeSuffix(precision);
