c0a2d9f49ce20dd22ee5de12fbb7beea42a299d9
Rafal Maciag <rafal.maciag@modelingevolution.com>
2025-02-21 22:20:34 +0100

    Giga Refactor Part 1.

--- src/ModelingEvolution.VideoStreaming.VectorGraphics/StreamingCanvasEngine.cs
+++ src/ModelingEvolution.VideoStreaming.VectorGraphics/StreamingCanvasEngine.cs
@@ -141,32 +141,3 @@ public class TransferWatch
 
     public override string ToString() => _lastBps.ToString();
 }
-public class FpsWatch
-{
-    private readonly Stopwatch _sw = Stopwatch.StartNew();
-    public TimeSpan MeasurePeriod = TimeSpan.FromSeconds(5);
-    private ulong _i = 0;
-    private double _lastFps;
-    public double Value => _lastFps;
-    public static explicit operator double(FpsWatch watch)
-    {
-        return watch._lastFps;
-    }
-    public static FpsWatch operator ++(FpsWatch watch)
-    {
-        watch._i++;
-        if (watch._sw.Elapsed <= watch.MeasurePeriod) return watch;
-        
-        watch._lastFps = watch._i * 1000.0 / watch._sw.Elapsed.TotalMilliseconds;
-        watch._sw.Restart();
-        watch._i = 0;
-        return watch;
-    }
-
-    public override string ToString() => Value.ToString();
-
-    public void Restart()
-    {
-        _sw.Restart();
-    }
-}
